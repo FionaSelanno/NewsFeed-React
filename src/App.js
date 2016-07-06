479a0c1154a78d681bb331e9aff36c4044821114
Fiona Selanno <Fiona@MacBook-Pro-van-Fiona.local>
2016-07-06 10:14:59 +0200

    start new React project NewsFeed

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import NewsFeed from './NewsFeed';
+
+class App extends React.Component {
+    render() {
+        return (
+            <NewsFeed/>
+        );
+    }
+}
+
+export default App;
