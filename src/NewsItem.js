0de839c1f9047dfb4c1df76878b8a2a591e848d6
Fiona Selanno <Fiona@MacBook-Pro-van-Fiona.local>
2016-07-06 12:06:48 +0200

    links to newsfeed:title, discription, image

---
+++ src/NewsItem.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import NewsFeed from './NewsFeed';
+
+class NewsItem extends React.Component {
+    render() {
+      console.log(this.props);
+
+        return (
+          <div>
+            <h2>{this.props.title}</h2>
+            <h3>{this.props.discription}</h3>
+            <p><a href={this.props.link}>Read article</a></p>
+            <img src={this.props.image} />
+            </div>
+        );
+    }
+}
+
+export default NewsItem;
